20a86a5956394d0f57be658d75436dfc3f08b644
Anurag Singh <anuragsingh2313@gmail.com>
2025-10-18 18:44:00 +0530

    minor changes

--- internal/db/queries/users.sql
+++ internal/db/queries/users.sql
@@ -2,7 +2,7 @@
 INSERT INTO users (id, email) VALUES ($1, $2) RETURNING id;
 
 -- name: GetUserByEmail :one
-SELECT id FROM users WHERE email = $1 AND deleted_at is NULL LIMIT 1;
+SELECT id FROM users WHERE email = $1 LIMIT 1;
 
 -- name: DeleteUser :exec
 UPDATE users SET deleted_at = NOW() WHERE id = $1 AND deleted_at is NULL;
